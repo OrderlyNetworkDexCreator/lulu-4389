918fad824e619ac6f04269f05af7e2ee1100e62f
Mario Reder <mario.reder@pm.me>
2025-12-18 16:00:59 +0100

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -18,7 +18,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_APP_DESCRIPTION": "LULU Exchange where you can trade meme  coins bitcoin and more",
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "true",
-  "VITE_ENABLED_MENUS": "Portfolio,Trading,Markets,Leaderboard,Swap,Rewards,Vaults",
+  "VITE_ENABLED_MENUS": "Portfolio,Trading,Markets,Swap,Rewards,Vaults",
   "VITE_CUSTOM_MENUS": "",
   "VITE_ENABLE_SERVICE_DISCLAIMER_DIALOG": "false",
   "VITE_ENABLE_CAMPAIGNS": "false",
